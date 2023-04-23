f8ef9c4cdbf1ea5af89411f085c257d6265494fb
nicholasL <nicklohmann@msn.com>
2023-04-23 04:05:21 -0500

    Refactor blog show function to include tags

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,47 +1,58 @@
 import { Blog } from "../models/blog.js"
+import { Tag } from "../models/tag.js"
 
 function newBlog(req, res) {
-  res.render('blogs/new' , {
+  res.render('blogs/new', {
     title: 'Add Post'
   })
 }
 
 function index(req, res) {
   Blog.find({})
-  .then(blogs => {
-    res.render('blogs' , {
-      blogs,
-      title: 'Blog Index'
+    .then(blogs => {
+      res.render('blogs', {
+        blogs,
+        title: 'Blog Index'
+      })
     })
-  })
     .catch(err => {
       console.log(err)
       console.log('INDEX ERROR');
       res.redirect('/')
     })
-  
+
 }
 
 function create(req, res) {
   req.body.creator = req.user.profile._id
   Blog.create(req.body)
-  .then(blog => {
-    res.redirect(`/blogs/`)
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/blogs/new')
-  })
+    .then(blog => {
+      res.redirect(`/blogs/`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/blogs/new')
+    })
 
 }
 function show(req, res) {
   Blog.findById(req.params.blogId)
-  .then(blog => {
-    res.render('blogs/show' , {
-      blog,
-      title: 'Blog Details'
+    .populate('tags')
+    .then(blog => {
+      Tag.find({ _id: { $nin: blog.tags } })
+        .then(tags => {
+          res.render('blogs/show', {
+            blog,
+            title: 'Blog Details',
+            tags
+          })
+        })
+        .catch(err => {
+          console.log(err)
+          res.sen("TAG INFO NOT FOUND")
+          res.redirect("/blogs")
+        })
     })
-  })
     .catch(err => {
       console.log(err)
       console.log('SHOW ERROR');
@@ -50,36 +61,36 @@ function show(req, res) {
 }
 function edit(req, res) {
   Blog.findById(req.params.blogId)
-  .then( blog=> {
-    res.render('blogs/edit', {
-      blog,
-      title: 'Edit Blog'
+    .then(blog => {
+      res.render('blogs/edit', {
+        blog,
+        title: 'Edit Blog'
+      })
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/blogs')
-  })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/blogs')
+    })
 }
 function update(req, res) {
-  Blog.findByIdAndUpdate(req.params.blogId, req.body, {new: true})
-  .then(blog => {
-    res.redirect(`/blogs/${blog._id}`)
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/blogs')
-  })
+  Blog.findByIdAndUpdate(req.params.blogId, req.body, { new: true })
+    .then(blog => {
+      res.redirect(`/blogs/${blog._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/blogs')
+    })
 }
 function deletePost(req, res) {
   Blog.findByIdAndDelete(req.params.blogId)
-  .then(blog => {
-    res.redirect('/blogs')
-  })
-  .catch(error => {
-    console.log(error);
-    res.redirect('/blogs')
-  })
+    .then(blog => {
+      res.redirect('/blogs')
+    })
+    .catch(error => {
+      console.log(error);
+      res.redirect('/blogs')
+    })
 
 }
 
