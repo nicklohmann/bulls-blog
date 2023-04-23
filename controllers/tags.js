ff244edba78c5ce9edbd8a1a46e4d33d3d15a28d
nicholasL <nicklohmann@msn.com>
2023-04-23 03:46:54 -0500

    add tag controller functions and view

--- controllers/tags.js
+++ controllers/tags.js
@@ -1,11 +1,28 @@
 import { Tag } from "../models/tag.js"
 
 function newTag(req, res) {
-
+  Tag.find({})
+  .then(tags => {
+    res.render('tags/add') , {
+      title: 'Add Tag',
+      tags
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/blogs')
+  })
 }
 
 function create(req, res) {
-
+  Tag.create(req.body)
+  .then(tag => {
+    res.redirect('/tags/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/tags/new')
+  })
 }
 
 
